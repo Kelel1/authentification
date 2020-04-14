9a18aa09387fae0590a0621f7fc1d9cdc5b8771b
Kern Elder <kelel1@yahoo.com>
2020-04-14 16:23:57 -0400

    implement pasport authentification functions

--- app.js
+++ app.js
@@ -19,10 +19,37 @@ const User = mongoose.model(
   })
 );
 
+
 const app = express();
 app.set("views", __dirname);
 app.set("view engine", "ejs");
 
+passport.use(
+    new LocalStrategy((username, password, done) => {
+      User.findOne({ username: username }, (err, user) => {
+        if (err) { 
+          return done(err);
+        };
+        if (!user) {
+          return done(null, false, { msg: "Incorrect username" });
+        }
+        if (user.password !== password) {
+          return done(null, false, { msg: "Incorrect password" });
+        }
+        return done(null, user);
+      });
+    })
+);
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+});
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
